aa7cc597fb9a2b22773b792c6909e33f5eb39117
YaweiZhao <zhaoyawei@nudt.edu.cn>
2023-01-30 12:05:50 +0800

    d

--- medicalDataHandler/handlePublicDatasets/luna16/classification.py
+++ medicalDataHandler/handlePublicDatasets/luna16/classification.py
@@ -1,14 +1,15 @@
 import csv
 import numpy as np
+import time
+import nibabel as nib
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
 from skimage.segmentation import clear_border
-from skimage.measure import label,regionprops, perimeter
-from skimage.morphology import ball, disk, dilation, binary_erosion, remove_small_objects, erosion, closing, reconstruction, binary_closing
-from skimage.filters import roberts, sobel
+from skimage.measure import label,regionprops
+from skimage.morphology import disk, binary_erosion, binary_closing
+from skimage.filters import roberts
 from scipy import ndimage as ndi
-import scipy.ndimage
-from skimage import measure, feature
+from skimage import measure
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
@@ -104,11 +105,21 @@ class Luna16(object):
         assert(isinstance(givenWorldCoord, WorldCoord))
         candidateOfNodule = Nodule(givenCtScan=givenCtScan, givenWorldCoordOfNodule=givenWorldCoord)
         return candidateOfNodule
+    
+    def extractCollectionOfCandidateOfNodule(self, givenMaxNumber = 100):
+        candidateList = []
+        candidateOfNodules = self.getCandidateNodules()
+        for candidateIndex, candidate in enumerate(candidateOfNodules[0:givenMaxNumber]):
+            ctScan = self.extractCtScan(givenSeriesId=candidate[0])
+            worldCoord = WorldCoord(givenX=float(candidate[1]), givenY=float(candidate[2]), givenZ=float(candidate[3])) 
+            nodule = self.extractCandidateOfNodule(givenCtScan=ctScan, givenWorldCoord=worldCoord)
+            candidateList.append(nodule)
+        return candidateList
 
     def saveCandidateOfNodule(self, givenTargetPath, givenNoduleList):
         for nodule in givenNoduleList:
             assert(isinstance(nodule, Nodule))
-            nodule.saveNodule(givenTargetPath=givenTargetPath, givenType='nii')
+            nodule.save(givenTargetPath=givenTargetPath)
     pass
 
 class WorldCoord(object):
@@ -153,14 +164,22 @@ class SettingAboutCtScan(object):
 
 class SettingAboutSlice(object):
     def __init__(self, givenSliceId):
-        self.__radiusOfBox = 20
-        self.__pad = 2
         self.__threshold = -400 # for mask
         self.__sliceId = givenSliceId
     
     def getThreshold(self):
         return self.__threshold
-    
+    pass
+
+class SettingAboutNodule(object):
+    def __init__(self, givenWorldCoordOfNodule):
+        self.__worldCoord = givenWorldCoordOfNodule
+        self.__radiusOfBox = 40
+        self.__pad = 2
+
+    def getWorldCoord(self):
+        return self.__worldCoord
+
     def getRadiusOfBox(self): # radiusOfBox是正方形边长一半
         return self.__radiusOfBox
 
@@ -168,12 +187,12 @@ class SettingAboutSlice(object):
         return self.__pad
     pass
 
-class SettingAboutNodule(object):
-    def __init__(self, givenWorldCoordOfNodule):
-        self.__worldCoord = givenWorldCoordOfNodule
-
-    def getWorldCoord(self):
-        return self.__worldCoord
+class IdGenerator(object):
+    def __init__(self):
+        self.__idNumber = time.time()
+    
+    def get(self):
+        return str(self.__idNumber)
     pass
 
 class Nodule(object):
@@ -181,15 +200,45 @@ class Nodule(object):
         assert(isinstance(givenCtScan, CtScan))
         self.__settingObj = SettingAboutNodule(givenWorldCoordOfNodule=givenWorldCoordOfNodule)
         self.__voxelCoord = VoxelCoord(givenWorldCoord=self.__settingObj.getWorldCoord(), givenOriginCoord=givenCtScan.getSetting().getOriginCoord(), givenSpacing=givenCtScan.getSetting().getSpacing())
+        self.__sliceImg = givenCtScan.extractSliceByCoord(givenWorldCoord=givenWorldCoordOfNodule)
+        self.__img = self.__initData()
     
     def getVoxelCoord(self):
         return self.__voxelCoord
 
-    def getWorldCoord(self):
-        return self.__settingObj.getWorldCoord()
+    def __initData(self):
+        x,y,z = self.__voxelCoord.get() # 注意 y代表纵轴，x代表横轴
+        noduleImg = self.__sliceImg[max(0, y - self.__settingObj.getRadiusOfBox()):min(self.__sliceImg.shape[0], y + self.__settingObj.getRadiusOfBox()),
+                                    max(0, x - self.__settingObj.getRadiusOfBox()):min(self.__sliceImg.shape[1], x + self.__settingObj.getRadiusOfBox())]
+        return noduleImg
     
-    def saveNodule(self, givenTargetPath, givenType = 'nii'):
-        xxx
+    def highlight2DByBox(self): 
+        x,y,z = self.__voxelCoord.get() # 注意 y代表纵轴，x代表横轴
+        self.__sliceImg[max(0, y - self.__settingObj.getRadiusOfBox()):min(self.__sliceImg.shape[0], y + self.__settingObj.getRadiusOfBox()),
+                        max(0, x - self.__settingObj.getRadiusOfBox() - self.__settingObj.getPad()):max(0, x - self.__settingObj.getRadiusOfBox())] = 3000 # 竖线
+
+        self.__sliceImg[max(0, y - self.__settingObj.getRadiusOfBox()):min(self.__sliceImg.shape[0], y + self.__settingObj.getRadiusOfBox()),
+                        min(self.__sliceImg.shape[1], x + self.__settingObj.getRadiusOfBox()):min(self.__sliceImg.shape[1], x + self.__settingObj.getRadiusOfBox() + self.__settingObj.getPad())] = 3000 # 竖线
+
+        self.__sliceImg[max(0, y - self.__settingObj.getRadiusOfBox() - self.__settingObj.getPad()):max(0, y - self.__settingObj.getRadiusOfBox()),
+                        max(0, x - self.__settingObj.getRadiusOfBox()):min(self.__sliceImg.shape[1], x + self.__settingObj.getRadiusOfBox())] = 3000 # 横线
+
+        self.__sliceImg[min(self.__sliceImg.shape[0], y + self.__settingObj.getRadiusOfBox()):min(self.__sliceImg.shape[0], y + self.__settingObj.getRadiusOfBox() + self.__settingObj.getPad()),
+                        max(0, x - self.__settingObj.getRadiusOfBox()):min(self.__sliceImg.shape[1], x + self.__settingObj.getRadiusOfBox())] = 3000 # 横线
+        plt.figure(1)
+        plt.imshow(self.__sliceImg, cmap='gray')
+        plt.show()
+
+    def save(self, givenTargetDir, givenType = 'nii'):
+        targetPath = givenTargetDir + '/' + IdGenerator().get() + '.' + givenType
+        if givenType == 'nii':
+            new_img = nib.nifti1.Nifti1Image(self.__img, None, header=None)
+            nib.save(new_img, targetPath)
+        if givenType == 'jpeg' or givenType == 'jpg':
+            plt.figure(1)
+            plt.imshow(self.__img, cmap='gray')
+            plt.axis('off')
+            plt.savefig(targetPath)
         pass
     pass
 
@@ -249,25 +298,6 @@ class Slice(object):
         binary = self.__closureForNoduleAttachedLungWall(givenBinaryImg=binary) # Step 6: Closure operation with a disk of radius 10. This operation is to keep nodules attached to the lung wall.
         return binary
 
-    def highlightNoduleByBox(self, givenNodule):
-        assert(isinstance(givenNodule, Nodule))
-        # 注意 y代表纵轴，x代表横轴
-        x,y,z = givenNodule.getVoxelCoord().get()
-        self.__image[max(0, y - self.__settingObj.getRadiusOfBox()):min(self.__image.shape[0], y + self.__settingObj.getRadiusOfBox()),
-        max(0, x - self.__settingObj.getRadiusOfBox() - self.__settingObj.getPad()):max(0, x - self.__settingObj.getRadiusOfBox())] = 3000 # 竖线
-
-        self.__image[max(0, y - self.__settingObj.getRadiusOfBox()):min(self.__image.shape[0], y + self.__settingObj.getRadiusOfBox()),
-        min(self.__image.shape[1], x + self.__settingObj.getRadiusOfBox()):min(self.__image.shape[1], x + self.__settingObj.getRadiusOfBox() + self.__settingObj.getPad())] = 3000 # 竖线
-
-        self.__image[max(0, y - self.__settingObj.getRadiusOfBox() - self.__settingObj.getPad()):max(0, y - self.__settingObj.getRadiusOfBox()),
-        max(0, x - self.__settingObj.getRadiusOfBox()):min(self.__image.shape[1], x + self.__settingObj.getRadiusOfBox())] = 3000 # 横线
-
-        self.__image[min(self.__image.shape[0], y + self.__settingObj.getRadiusOfBox()):min(self.__image.shape[0], y + self.__settingObj.getRadiusOfBox() + self.__settingObj.getPad()),
-        max(0, x - self.__settingObj.getRadiusOfBox()):min(self.__image.shape[1], x + self.__settingObj.getRadiusOfBox())] = 3000 # 横线
-        plt.figure(1)
-        plt.imshow(self.__image, cmap='gray')
-        plt.show()
-
     def show(self):
         plt.figure(1)
         plt.imshow(self.__image)
@@ -326,14 +356,10 @@ class Test(object):
         pass
     
     def execute(self):
-        luna16 = Luna16(givenFilePathOfUid='/home/yawei/MedicalDatasets/CSVFILES/seriesuids.csv', 
-        givenFilePathOfCandidateNodules='/home/yawei/MedicalDatasets/CSVFILES/candidates.csv',
-        givenFilePathOfAnnotations='/home/yawei/MedicalDatasets/CSVFILES/annotations.csv')
-        cts = CtScan(givenFilePath="/home/yawei/my-flbenchmark/toyData/mhd/1.3.6.1.4.1.14519.5.2.1.6279.6001.102681962408431413578140925249.mhd")
-        worldCorrdOfNodule = WorldCoord(givenX=105.0558,givenY=19.82526, givenZ=-91.2478)
-        nodule = Nodule(givenCtScan=cts,givenWorldCoordOfNodule=worldCorrdOfNodule) 
-        x, y, z = VoxelCoord(givenWorldCoord=worldCorrdOfNodule,givenOriginCoord=cts.getSetting().getOriginCoord(),givenSpacing=cts.getSetting().getSpacing()).get()
-        Slice(givenCtScan=cts, givenSliceId=z).highlightNoduleByBox(givenNodule=nodule)
+        luna16 = Luna16(givenDir='/home/yawei/Documents/LUNA16')
+        candidateList = luna16.extractCollectionOfCandidateOfNodule(givenMaxNumber=10)
+        for candidateNodule in candidateList:
+            candidateNodule.save(givenTargetDir='/home/yawei/Documents/LUNA16/candidateOfnodules', givenType='jpeg')
         pass
     pass
 
